bee9719811a61b50c9409a2bbf1600cda9c1d6a0
Braden Gerace <braden.gerace@hotmail.com>
2023-03-09 11:17:19 -0500

    typing added

--- wordle-1/src/hooks/useWordle.js
+++ wordle-1/src/hooks/useWordle.js
@@ -22,8 +22,32 @@ const useWordle = (solution) => {
 
   // handle keyup event & track current guess
   // if user presses enter, add the new guess
-  const handleKeyup = () => {
+  const handleKeyup = ({ key }) => {
+    if (key === 'Enter') {
+      if (turn > 5) {
 
+      }
+      if (history.includes(currentGuess)) {
+
+      }
+      if (currentGuess.length !== 5) {
+        
+      }
+    }
+    if (key === 'Backspace') {
+      setCurrentGuess((prev) => {
+        return prev.slice(0, -1)
+      })
+      return
+    }
+
+    if (/^[A-Za-z]$/.test(key)) {
+      if (currentGuess.length < 5) {
+        setCurrentGuess((prev) => {
+          return prev + key
+        })
+      }
+    }
   }
 
   return {turn, currentGuess, guesses, isCorrect, handleKeyup}
