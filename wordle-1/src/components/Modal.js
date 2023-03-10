418867eacd6eeb138ea2afb75da72dde84bcaa29
Braden Gerace <braden.gerace@hotmail.com>
2023-03-10 16:59:34 -0500

    final

--- wordle-1/src/components/Modal.js
+++ wordle-1/src/components/Modal.js
@@ -6,14 +6,16 @@ export default function Modal({ isCorrect, turn, solution }) {
         {isCorrect && (
             <div>
                 <h1>You Win!</h1>
-                <p className='solution'>The word was: {solution}</p>
+                <p>The word was: </p>
+                <p className='solution'>{solution}</p>
                 <p>You found the solution in {turn} guesses!</p>
             </div>
         )}
         {!isCorrect && (
             <div>
                 <h1>You Lose!</h1>
-                <p className='solution'>The word was: {solution}</p>
+                <p>The word was: </p>
+                <p className='solution'>{solution}</p>
                 <p>Trash!</p>
             </div>
         )}
